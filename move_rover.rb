42c57b618638c4ded5a9d83071af001b0b15c715
hov8aa <hov8aa@gmail.com>
2024-10-01 19:37:04 +0530

    not able to break down the problem into smallest parts that I can try to search and solve step by step

---
+++ move_rover.rb
@@ -0,0 +1,9 @@
+Class move_rover()
+
+    def initialize (x,y,f)
+        @x_coordinate = x
+        @y_coordinate = y 
+        @facing = f
+    end
+
+end
--- spec/move_rover_spec.rb
+++ spec/move_rover_spec.rb
@@ -1,9 +1,19 @@
 require_relative '../move_rover'
 
-RSpec.describe "write code to more rover & test with smallest specifications" do
+RSpec.describe move_rover do
 
 # What to test for
 # 1. What happens in the happy path?
+#   Test case 1:
+#   If the input/instruction is L, the rover successfully turns Left
+
+
+#   Test case 2:
+#   If the input/instruction is R, the rover successfully turns Right
+
+
+#   Test case 3:
+#   If the input/instruction is M, the rover successfully moves 1 step forward
 
 
 # 2. What happens when I create an error or add invalid input? (check the validation)
