39ecb1fa754974d0f503002d91d9a1a52244067a
hov8aa <hov8aa@gmail.com>
2024-10-01 16:00:32 +0530

    trying to figure out, what is that I need to test for and how do I write my first specifications

---
+++ spec/move_rover_spec.rb
@@ -0,0 +1,11 @@
+require_relative '../move_rover'
+
+RSpec.describe "write code to more rover & test with smallest specifications" do
+
+# What to test for
+# 1. What happens in the happy path?
+
+
+# 2. What happens when I create an error or add invalid input? (check the validation)
+
+end
