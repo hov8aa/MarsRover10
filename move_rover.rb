b542004b338670428850801f2f0058024860fda1
hov8aa <hov8aa@gmail.com>
2024-10-04 22:08:57 +0530

    Upgraded function turn_rover to turn the rover but my test cases are not testing for the right thing. How do I test the right thing?

--- move_rover.rb
+++ move_rover.rb
@@ -9,16 +9,43 @@ def move_rover
     puts "enter the face direction of the rover"
     rover_face = gets.chomp
 
-    puts "enter instructions to move left or right consequtively as LRLRLR"
-    instructions = gets.chomp
+    puts "enter instructions to move left or right consequtively as left / right"
+    instruction = gets.chomp
 
-    turn_rover(instructions, rover_face)
+    turn_rover(instruction, rover_face)
 end
 
-def turn_rover(instructions, rover_face)
-    instructions.each { |c|
-        #puts "turned left with instruction #{instruction}"
-    }
+def turn_rover(instruction, rover_face)
+    puts "Rover face before turning #{instruction} is #{rover_face}"
+    if instruction == "left"
+        if rover_face == "North"
+            new_rover_face = "West"
+        end
+        if rover_face == "West"
+            new_rover_face = "South"
+        end
+        if rover_face == "South"
+            new_rover_face = "East"
+        end
+        if rover_face == "East"
+            new_rover_face = "North"
+        end
+    end
+    if instruction == "right"
+        if rover_face == "North"
+            new_rover_face = "East"
+        end
+        if rover_face == "East"
+            new_rover_face = "South"
+        end
+        if rover_face == "South"
+            new_rover_face = "West"
+        end
+        if rover_face == "West"
+            new_rover_face = "North"
+        end
+    end    
+    puts "Rover face after turning #{instruction} is #{new_rover_face}"
 end
 
 move_rover
