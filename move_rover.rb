dac41815d3c05df1cfb89ab5cef7b102973c1053
hov8aa <hov8aa@gmail.com>
2024-10-08 21:50:29 +0530

    I think, i've just be successful at writing one minute incremental specification and validate it by writing a code to execute the same, 13 test cases and they all run successfully.

--- move_rover.rb
+++ move_rover.rb
@@ -1,35 +1,52 @@
 def turn_rover_left(direction)
-    if direction == 'n'
-        return 'w'
-    elsif direction == 'w'
-        return 's'
-    elsif direction == 's'
-        return 'e'
-    elsif direction == 'e'
-        return 'n'
+    if direction == 'N'
+        return 'W'
+    elsif direction == 'W'
+        return 'S'
+    elsif direction == 'S'
+        return 'E'
+    elsif direction == 'E'
+        return 'N'
     end
 end
 
 def turn_rover_right(direction)
-    if direction == 'n'
-        return 'e'
-    elsif direction == 'e'
-        return 's'
-    elsif direction == 's'
-        return 'w'
-    elsif direction == 'w'
-        return 'n'
+    if direction == 'N'
+        return 'E'
+    elsif direction == 'E'
+        return 'S'
+    elsif direction == 'S'
+        return 'W'
+    elsif direction == 'W'
+        return 'N'
     end
 end
 
 def move_forward(x,y,direction)
-    if direction == 'n'
+    if direction == 'N'
         return 'x,y+1'
-    elsif direction == 'e'
+    elsif direction == 'E'
         return 'x+1,y'
-    elsif direction == 's'
+    elsif direction == 'S'
         return 'x,y-1'
-    elsif direction == 'w'
+    elsif direction == 'W'
         return 'x-1,y'
     end
+end
+
+def continuous_movement(instructions, rover_face)
+    #return 'N'
+    #expect(continuous_movement("LMLMLMLMM","N")).to eq('N')
+    i = 0
+    while i < instructions.length do
+        if instructions[i] == 'L'
+            rover_face = turn_rover_left(rover_face)
+        elsif instructions[i] == 'R'
+            rover_face = turn_rover_right(rover_face)
+        elsif instructions[i] == 'M'
+            #DoNothingForNow
+        end
+        i = i+1
+    end
+    return rover_face
 end
--- spec/move_rover_spec.rb
+++ spec/move_rover_spec.rb
@@ -4,61 +4,61 @@ RSpec.describe "move_rover" do
     
     context "turn left" do
         it "should change from north to west for turn left" do
-            expect(turn_rover_left("n")).to eq("w")
+            expect(turn_rover_left("N")).to eq("W")
         end
 
         it "should change from west to south for turn left" do
-            expect(turn_rover_left("w")).to eq("s")
+            expect(turn_rover_left("W")).to eq("S")
         end
 
         it "should change from south to east for turn left" do
-            expect(turn_rover_left("s")).to eq("e")
+            expect(turn_rover_left("S")).to eq("E")
         end
 
         it "should change from east to north for turn left" do
-            expect(turn_rover_left("e")).to eq("n")
+            expect(turn_rover_left("E")).to eq("N")
         end
     end
 
     context "turn right" do
         it "should change from north to east for turn right" do
-            expect(turn_rover_right("n")).to eq("e")
+            expect(turn_rover_right("N")).to eq("E")
         end
 
         it "should change from east to south for turn right" do
-            expect(turn_rover_right("e")).to eq("s")
+            expect(turn_rover_right("E")).to eq("S")
         end
 
         it "should change from south to west for turn right" do
-            expect(turn_rover_right("s")).to eq("w")
+            expect(turn_rover_right("S")).to eq("W")
         end
 
         it "should change from west to north for turn right" do
-            expect(turn_rover_right("w")).to eq("n")
+            expect(turn_rover_right("W")).to eq("N")
         end
     end
 
     context "move rover forward" do
         it "moves rover one step forward in north direction" do
-            expect(move_forward('x','y','n')).to eq('x,y+1')
+            expect(move_forward('x','y','N')).to eq('x,y+1')
         end
 
         it "moves rover one step forward in west direction" do
-            expect(move_forward('x','y','w')).to eq('x-1,y')
+            expect(move_forward('x','y','W')).to eq('x-1,y')
         end
 
         it "moves rover one step forward in south direction" do
-            expect(move_forward('x','y','s')).to eq('x,y-1')
+            expect(move_forward('x','y','S')).to eq('x,y-1')
         end
 
         it "moves rover one step forward in east direction" do
-            expect(move_forward('x','y','e')).to eq('x+1,y')
+            expect(move_forward('x','y','E')).to eq('x+1,y')
         end
     end
 
     context "when the instructions are in the form of a sequence of letters" do
         it "should find the final rover facing direction if the original face is north" do
-            expect(continuous_movement("LMLMLMLMM")).to eq('N')
+            expect(continuous_movement("LMLMLMLMM","N")).to eq('N')
         end
     end
 end
